74b9e932c2ad030c433185538a07e506a28c61ce
ashutosh22102 <ashutoshs22102@iiitnr.edu.in>
2025-08-25 15:08:02 +0530

    assignment-1 done

--- Assignment-1/movie_search.py
+++ Assignment-1/movie_search.py
@@ -2,12 +2,20 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load dataset and create embeddings (global for testing)
 
-# Load the Sentence Transformer model
+df = pd.read_csv("movies.csv")
 
-# Convert the 'plot of the movies into an embedding
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
+
+df["embeddings"] = df["plot"].apply(lambda x: model.encode(x))
 
 def search_movies(query, top_n=5):
-    #TO_DO logic for implementing
+    """
+    Returns top_n movie matches for a given query
+    """
+    query_embedding = model.encode(query)
+    similarities = cosine_similarity([query_embedding], list(df["embeddings"]))[0]
+    df["similarity"] = similarities
+    results = df.sort_values("similarity", ascending=False).head(top_n)
+    return results[["title", "plot", "similarity"]]
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This repository contains the materials related to the "AI Systems development"
+This repository contains the Solutions related to the "AI Systems development"
